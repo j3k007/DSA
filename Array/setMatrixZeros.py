8124210691d214ba9da0b97f4f50f4cc7351ac07
Jatin Koli <70892665+j3k007@users.noreply.github.com>
2024-08-13 15:47:00 +0530

    Completed

--- Array/setMatrixZeros.py
+++ Array/setMatrixZeros.py
@@ -1,3 +1,4 @@
+# First Solution
 class Solution:
     def setZeroes(self, matrix: list[list[int]])-> None:
         """
@@ -51,3 +52,34 @@ class Solution:
                 matrix[0][i] = 0
 
 
+
+
+
+# Second Solution
+class Solution:
+    def setZeroes(self, matrix: list[list[int]])-> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        # determine which row/col needs to be zero
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
